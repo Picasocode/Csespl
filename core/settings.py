cfe662e4c3c7eb92e59772ac0528f72d57ea6a17
Picasocode <127660456+Picasocode@users.noreply.github.com>
2024-06-13 13:53:56 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -46,8 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.mi
-ddleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
