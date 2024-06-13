7e56a452745ee4b0e707b0b69bf1ac832296fe0b
Picasocode <127660456+Picasocode@users.noreply.github.com>
2024-06-13 10:25:11 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('', admin.site.urls),
     path('',include('app.urls')),
     path('',include('project.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
